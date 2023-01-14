aea14bb8c16d483a41152fc775353343f6d2f179
Fabio Adaniya <f1hiroki@outlook.com>
2023-01-14 18:59:09 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
-	$server = "localhost";
-    	$port = "3306";
-	$user = "root";
-	$password = "1234";
-	$database = "produto_php";
+    $server = "localhost";
+    $port = "3306";
+    $user = "root";
+    $password = "1234";
+    $database = "produto_php";
 
-	$con = mysqli_connect($server, $user, $password, $database, $port);
+    $con = mysqli_connect($server, $user, $password, $database, $port);
 ?>
